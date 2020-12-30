3202f3290d75217f0997b30da0db2e33d540c52b
Jaela-C <jaelacarlosama3c@hotmail.com>
2020-12-30 17:44:18 -0500

    #19 Resources

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
+
 class UserController extends Controller
 {
     public function authenticate(Request $request)
--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use App\Category;
 use Illuminate\Database\Seeder;
 
